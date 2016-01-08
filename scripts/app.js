8a694397d9043d269ba7923a15d24fa53b38af95
Anita Amini <baadge29@yahoo.co.uk>
2016-01-08 23:39:34 +0000

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope, dataService){
+	
+	$scope.addTodo = function(){
+		var todo = {name: "new task"};
+		$scope.todos.unshift(todo);
+	};
+	
+	dataService.getTodos(function(response){
+		$scope.todos = response.data;
+	});
+	
+	
+	$scope.deleteTodo = function(todo, index){
+		dataService.deleteTodo(todo);
+		$scope.todos.splice(index, 1);
+	};
+	
+	$scope.saveTodos = function(){
+		var filteredTodos = $scope.todos.filter(function(todo){
+			if (todo.edited) return todo;
+		});
+		dataService.saveTodos(filteredTodos);
+	};
+}).service('dataService', function($http){
+	this.getTodos = function(callback){ 
+		$http.get('mock/todos.json').then(callback);	
+	};
+	this.deleteTodo = function(){
+	};
+	
+	this.saveTodos = function(todos) {
+		console.log(todos.length);
+		
+	};
+});
